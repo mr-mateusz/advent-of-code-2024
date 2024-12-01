f76000bd8a89d5c30a091ed0588baac19d623582
mr-mateusz <37747987+mr-mateusz@users.noreply.github.com>
2024-12-01 11:21:56 +0100

    day_01 part 2

--- day_01/solution.py
+++ day_01/solution.py
@@ -1,5 +1,3 @@
-
-
 if __name__ == "__main__":
     path = 'input.txt'
 
@@ -11,11 +9,27 @@ if __name__ == "__main__":
     
     l1 = [int(n) for n in l1]
     l2 = [int(n) for n in l2]
-    
-    l1 = sorted(l1)
-    l2 = sorted(l2)
 
-    diffs = [n1 - n2 if n1 > n2 else n2 - n1 for n1, n2 in zip(l1, l2)]
+    diffs = [n1 - n2 if n1 > n2 else n2 - n1 for n1, n2 in zip(sorted(l1), sorted(l2))]
     
     # Part 1
     print(sum(diffs))
+
+    l2_counts = {}
+
+    for n in l2:
+        try:
+            l2_counts[n] += 1
+        except KeyError:
+            l2_counts[n] = 1
+
+    total = 0
+    for n in l1:
+        try:
+            score =n * l2_counts[n]
+        except KeyError:
+            score = 0
+        total += score
+
+    # Part 2
+    print(total)
