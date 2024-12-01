937aaa6c28d9b75a4c9db04b837f89f48cc8c8a2
mr-mateusz <37747987+mr-mateusz@users.noreply.github.com>
2024-12-01 11:22:51 +0100

    code reformat

--- day_01/solution.py
+++ day_01/solution.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
 
     data = [d.strip().split() for d in data]
     l1, l2 = list(zip(*data))
-    
+
     l1 = [int(n) for n in l1]
     l2 = [int(n) for n in l2]
 
     diffs = [n1 - n2 if n1 > n2 else n2 - n1 for n1, n2 in zip(sorted(l1), sorted(l2))]
-    
+
     # Part 1
     print(sum(diffs))
 
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     total = 0
     for n in l1:
         try:
-            score =n * l2_counts[n]
+            score = n * l2_counts[n]
         except KeyError:
             score = 0
         total += score
