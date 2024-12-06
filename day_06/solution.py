c6919fc5d17f60653388f93b796d29eac5be2230
mr-mateusz <37747987+mr-mateusz@users.noreply.github.com>
2024-12-06 21:38:19 +0100

    day_06 part 2

--- day_06/solution.py
+++ day_06/solution.py
@@ -1,3 +1,4 @@
+import time
 from enum import Enum
 from typing import Any, NamedTuple
 
@@ -28,8 +29,8 @@ class Direction(Enum):
 
 
 def find_start(grid: list[list]) -> Pos:
-    for _r in range(len(data)):
-        for _c in range(len(data[0])):
+    for _r in range(len(original_grid)):
+        for _c in range(len(original_grid[0])):
             if grid[_r][_c] == '^':
                 return Pos(_r, _c)
     return Pos(-1, -1)
@@ -37,8 +38,8 @@ def find_start(grid: list[list]) -> Pos:
 
 def count_values(grid: list[list], value: Any) -> int:
     total = 0
-    for r in range(len(data)):
-        for c in range(len(data[0])):
+    for r in range(len(original_grid)):
+        for c in range(len(original_grid[0])):
             if grid[r][c] == value:
                 total += 1
     return total
@@ -55,17 +56,49 @@ def move(grid: list[list], pos: Pos, direction: Direction) -> tuple[Pos, Directi
     return next_move, direction
 
 
-def simulate_movement(grid: list[list], start_pos: Pos) -> list[list]:
-    grid = [line[:] for line in grid]
+def deepcopy(lst: list[list]) -> list[list]:
+    return [line[:] for line in lst]
+
+
+def simulate_movement(grid: list[list], start_pos: Pos) -> tuple[list[list], bool]:
+    """Returned bool is an indication if guard is stuck in a loop."""
+    grid = deepcopy(grid)
 
     pos = start_pos
     direction = Direction.N
 
+    visited_positions = set()
     while pos.is_in(grid):
+        visited_positions.add((pos, direction))
         grid[pos.r][pos.c] = 'X'
         pos, direction = move(grid, pos, direction)
+        if (pos, direction) in visited_positions:
+            # The guard is stuck in an infinite loop
+            return grid, True
 
-    return grid
+    return grid, False
+
+
+def find_infinite_loops(original_grid: list[list], grid_with_movement: list[list], start_pos: Pos) -> int:
+    grid_with_movement = deepcopy(grid_with_movement)
+
+    # Starting position cannot be modified
+    grid_with_movement[start_pos.r][start_pos.c] = '^'
+
+    infinite_loops = 0
+    for _r in range(len(original_grid)):
+        for _c in range(len(original_grid[0])):
+            # This field was visited by the guard in the original grid, so we can try to modify it
+            if grid_with_movement[_r][_c] == 'X':
+                original_grid_modified = deepcopy(original_grid)
+                original_grid_modified[_r][_c] = '#'
+
+                # Simulate the guard's movement
+                _, is_stuck = simulate_movement(original_grid_modified, start_pos)
+                if is_stuck:
+                    infinite_loops += 1
+
+    return infinite_loops
 
 
 if __name__ == "__main__":
@@ -74,13 +107,22 @@ if __name__ == "__main__":
     with open(path, "r") as f:
         data = [l.strip() for l in f.readlines()]
 
-    data = [list(l) for l in data]
+    original_grid = [list(l) for l in data]
 
-    start = find_start(data)
+    start = find_start(original_grid)
 
-    grid_with_movement = simulate_movement(data, start)
+    grid_with_movement, _ = simulate_movement(original_grid, start)
 
     total = count_values(grid_with_movement, 'X')
 
     # Part 1
     print(total)
+
+    st = time.perf_counter()
+
+    infinite_loops = find_infinite_loops(original_grid, grid_with_movement, start)
+
+    print(f'Time: {time.perf_counter() - st}')
+
+    # part 2
+    print(infinite_loops)
