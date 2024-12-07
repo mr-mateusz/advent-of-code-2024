02ec0693cee21ee1dbdc1734de81de383dd690a2
mr-mateusz <37747987+mr-mateusz@users.noreply.github.com>
2024-12-07 11:29:46 +0100

    day_07 part 1

---
+++ day_07/solution.py
@@ -0,0 +1,40 @@
+def is_ok(line: tuple[int, list[int]]) -> bool:
+    """Input tuple: (expected_result, [values])"""
+    stack = [line]
+
+    while stack:
+        to_check = stack.pop()
+        expected, values = to_check
+
+        if len(values) > 1:
+            fv, sv = values[0], values[1]
+            if fv <= expected:
+                # If values are equal we still check in case of 0 (for addiction) and 1 (for multiplication)
+                stack.append((expected, [fv + sv] + values[2:]))
+                stack.append((expected, [fv * sv] + values[2:]))
+            if fv > expected:
+                # The value is already too big, we don't check further in this "branch"
+                pass
+        if len(values) == 1:
+            fv = values[0]
+            if fv == expected:
+                return True
+    return False
+
+
+if __name__ == '__main__':
+    path = "input.txt"
+
+    with open(path, 'r') as f:
+        data = [l.strip() for l in f.readlines()]
+
+    data = [l.split(': ') for l in data]
+    data = [(int(res), [int(v) for v in vals.split()]) for res, vals in data]
+
+    total = 0
+    for line in data:
+        if is_ok(line):
+            total += line[0]
+
+    # Part 1
+    print(total)
