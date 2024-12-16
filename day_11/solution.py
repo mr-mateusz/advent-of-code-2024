0d16fdf08efb51e191664099cc8c76dd7182d72a
mr-mateusz <37747987+mr-mateusz@users.noreply.github.com>
2024-12-16 20:00:35 +0100

    day_11 part 2

--- day_11/solution.py
+++ day_11/solution.py
@@ -1,3 +1,6 @@
+from functools import cache
+
+
 def blink(stone_vals: list[str]) -> list[str]:
     new_vals = []
     for val in stone_vals:
@@ -16,6 +19,23 @@ def blink(stone_vals: list[str]) -> list[str]:
     return new_vals
 
 
+@cache
+def stones_num(val: str, blinks: int) -> int:
+    if blinks == 0:
+        return 1
+
+    if val == '0':
+        return stones_num('1', blinks - 1)
+    if len(val) % 2 == 0:
+        half = len(val) // 2
+        fv = val[:half]
+        sv = val[half:].lstrip('0')
+        if not sv:
+            sv = '0'
+        return stones_num(fv, blinks - 1) + stones_num(sv, blinks - 1)
+    return stones_num(str(int(val) * 2024), blinks - 1)
+
+
 if __name__ == "__main__":
     path = "input.txt"
 
@@ -26,8 +46,16 @@ if __name__ == "__main__":
 
     stone_vals = data[:]
 
-    for _ in range(25):
+    for i in range(25):
         stone_vals = blink(stone_vals)
 
     # Part 1
     print(len(stone_vals))
+
+    stone_vals = data[:]
+
+    total_stones = 0
+    for v in stone_vals:
+        total_stones += stones_num(v, 75)
+
+    print(total_stones)
